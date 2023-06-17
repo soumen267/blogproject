62fc9e64acc7172304c974cec232198476d70c1c
soumen267 <57955941+soumen267@users.noreply.github.com>
2023-06-17 17:03:14 +0530

    changes

--- blog_db.sql
+++ blog_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2023 at 06:26 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Jun 17, 2023 at 01:32 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `city_table` (
   `CID` int(11) NOT NULL,
   `city` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `city_table`
@@ -54,7 +54,7 @@ CREATE TABLE `employeeposition` (
   `Position` varchar(100) DEFAULT NULL,
   `DateOfJoining` date NOT NULL,
   `Salary` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `employeeposition`
@@ -81,7 +81,7 @@ CREATE TABLE `marks` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `marks`
@@ -107,7 +107,7 @@ CREATE TABLE `student` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `student`
@@ -132,7 +132,7 @@ CREATE TABLE `student_table` (
   `name` varchar(100) NOT NULL,
   `Age` int(11) NOT NULL,
   `City` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `student_table`
@@ -154,7 +154,7 @@ CREATE TABLE `tbl_category` (
   `cat_name` varchar(100) NOT NULL,
   `updated_at` date DEFAULT current_timestamp(),
   `deleted_at` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_category`
@@ -165,7 +165,8 @@ INSERT INTO `tbl_category` (`id`, `cat_name`, `updated_at`, `deleted_at`) VALUES
 (5, 'Cats', NULL, NULL),
 (47, 'Javascript', '2023-05-31', NULL),
 (48, 'Jquery', '2023-06-03', NULL),
-(49, 'Framework', '2023-06-08', NULL);
+(49, 'Framework', '2023-06-08', NULL),
+(53, 'Laravels', '2023-06-15', NULL);
 
 -- --------------------------------------------------------
 
@@ -181,7 +182,7 @@ CREATE TABLE `tbl_comments` (
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_comments`
@@ -193,7 +194,8 @@ INSERT INTO `tbl_comments` (`id`, `post_id`, `user_id`, `comment_content`, `crea
 (3, 1, 0, '<p>Jhhhhhhh</p>', '2023-06-10 20:18:51', NULL, NULL),
 (4, 2, 0, '<p><span style=\"color: rgb(16, 28, 84); font-family: &quot;Source Sans Pro&quot;, sans-serif; font-size: 16px; letter-spacing: 0.4px;\">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</span><br></p>', '2023-06-10 21:33:04', NULL, NULL),
 (5, 2, 0, '<p>Is this good</p>', '2023-06-11 21:50:51', NULL, NULL),
-(6, 1, 3, '<p>Good</p>', '2023-06-11 21:55:57', NULL, NULL);
+(6, 1, 3, '<p>Good</p>', '2023-06-11 21:55:57', NULL, NULL),
+(7, 2, 0, '<p>Niceeee</p>', '2023-06-15 19:41:49', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -210,7 +212,7 @@ CREATE TABLE `tbl_employeeinfo` (
   `Address` text NOT NULL,
   `DOB` date NOT NULL,
   `GENDER` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_employeeinfo`
@@ -239,25 +241,26 @@ CREATE TABLE `tbl_posts` (
   `post_image` varchar(100) DEFAULT NULL,
   `post_content` longtext DEFAULT NULL,
   `post_tag` varchar(100) DEFAULT NULL,
-  `post_comment_count` varchar(100) DEFAULT NULL,
-  `post_status` varchar(100) DEFAULT NULL,
-  `post_view_count` varchar(100) DEFAULT NULL,
+  `post_comment_count` varchar(100) DEFAULT '0',
+  `post_status` varchar(100) DEFAULT 'draft',
+  `post_view_count` varchar(100) DEFAULT '0',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_posts`
 --
 
 INSERT INTO `tbl_posts` (`id`, `post_cat_id`, `post_title`, `post_author`, `post_user`, `post_date`, `post_image`, `post_content`, `post_tag`, `post_comment_count`, `post_status`, `post_view_count`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 5, 'Blog 1', 'Dyan Sheikh', '1', '2023-06-10', '64846a8eadbe2.jpg', 'Here are many variations of passages of <b>Lorem Ipsum available</b>, but the <font color=\"#ff0000\">majority have suffered alteration in some form</font>, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'aa', '0', 'aa', '0', '2023-06-03 15:30:25', '2023-06-11 11:15:06', NULL),
-(2, 5, 'Blog 2', 'Kelvin Clein', '2', '2023-06-10', '64846a996f4a0.jpg', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'aa', '0', 'aa', '0', '2023-06-07 00:00:00', '2023-06-11 11:32:42', NULL),
-(3, 5, 'Blog 3', 'Matt Heardy', '3', '2023-06-10', '64846aa4c9ded.jpg', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'aa', '0', 'aa', '0', '1900-01-02 00:00:00', '2023-06-11 11:30:40', NULL),
-(11, 5, 'Blog 42', 'Brett Watt', '3', '2023-06-10', '64846afa4f8bf.jpg', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Test, Post', '0', 'Draft', '0', '1900-01-04 00:00:00', '2023-06-11 11:30:31', NULL),
-(12, 49, 'Blog 5', 'John Diwin', '1', '2023-06-11', '6485d75172ec2.jpg', '<p>Lorem ipsum</p>', 'ds', '0', 'draft', '0', '2023-06-10 19:09:21', '2023-06-11 14:16:49', NULL),
-(13, 48, 'Blog 6', 'Brett Watts', '1', '2023-06-11', '6485d76ff0129.jpg', '<p>UUUU</p>', 'ds', '0', 'draft', '0', '2023-06-10 19:11:10', '2023-06-11 14:17:19', NULL);
+(1, 5, 'Blog 1', 'Dyan Sheikh', '1', '2023-06-10', '64846a8eadbe2.jpg', 'Here are many variations of passages of <b>Lorem Ipsum available</b>, but the <font color=\"#ff0000\">majority have suffered alteration in some form</font>, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'aa', '0', 'draft', '0', '2023-06-03 15:30:25', '2023-06-15 05:10:25', NULL),
+(2, 5, 'Blog 2', 'Kelvin Clein', '2', '2023-06-10', '64846a996f4a0.jpg', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'aa', '0', 'draft', '0', '2023-06-07 00:00:00', '2023-06-15 05:10:22', NULL),
+(3, 5, 'Blog 3', 'Matt Heardy', '3', '2023-06-10', '64846aa4c9ded.jpg', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'aa', '0', 'draft', '0', '1900-01-02 00:00:00', '2023-06-15 05:10:18', NULL),
+(11, 5, 'Blog 42', 'Brett Watt', '3', '2023-06-10', '64846afa4f8bf.jpg', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Test, Post', '0', 'draft', '0', '1900-01-04 00:00:00', '2023-06-15 05:10:10', NULL),
+(12, 49, 'Blog 5', 'John Diwinqq', '1', '2023-06-11', '6485d75172ec2.jpg', '<p>Lorem ipsum</p>', 'ds', '0', 'draft', '0', '2023-06-10 19:09:21', '2023-06-15 13:21:47', NULL),
+(13, 48, 'Blog 6', 'Brett Watts', '1', '2023-06-11', '6485d76ff0129.jpg', '<p>UUUU</p>', 'ds', '0', 'draft', '0', '2023-06-10 19:11:10', '2023-06-11 14:17:19', NULL),
+(14, 48, 'Jq', 'McDanniel', '1', '2023-06-15', '648a9e0167aa2.jpg', '<p>Lorem</p>', 'JQ', '0', 'draft', '0', '2023-06-15 10:41:08', '2023-06-15 05:13:37', NULL);
 
 -- --------------------------------------------------------
 
@@ -271,7 +274,7 @@ CREATE TABLE `tbl_register` (
   `password` varchar(100) NOT NULL,
   `cpassword` varchar(100) NOT NULL,
   `deleted_at` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_register`
@@ -293,23 +296,25 @@ CREATE TABLE `tbl_users` (
   `user_firstname` varchar(100) NOT NULL,
   `user_lastname` varchar(100) NOT NULL,
   `user_email` varchar(100) NOT NULL,
-  `user_role` varchar(100) NOT NULL,
+  `user_role` varchar(100) NOT NULL DEFAULT 'user',
   `user_image` varchar(100) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT '1',
   `created_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updateed_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
 INSERT INTO `tbl_users` (`user_id`, `username`, `password`, `user_firstname`, `user_lastname`, `user_email`, `user_role`, `user_image`, `status`, `created_at`, `updateed_at`, `deleted_at`) VALUES
-(1, 'somu1234', 'Soumen@123456677', 'Soumen', 'Das', 'soumen13@gmail.com', 'admin', '6481e60ce903d.jpg', '1', '2023-06-08 21:05:25', '2023-06-08 21:05:25', NULL),
+(1, 'somu12345', 'Soumen@123456677', 'Soumen', 'Das', 'soumen13@gmail.com', 'admin', '6481e60ce903d.jpg', '1', '2023-06-15 18:39:31', '2023-06-15 18:39:31', NULL),
 (2, 'dan12345', 'Daniel@123456789', 'Daniel', 'Creg', 'd124@gmail.com', 'admin', '6481e62608392.jpg', '1', '2023-06-08 21:05:28', '2023-06-08 21:05:28', NULL),
 (3, 'somu123456', 'Soumen@456137899', 'Josh', 'dyne', 'josh@gmail.com', 'user', '6481e62f14fe7.jpg', '1', '2023-06-08 21:05:30', '2023-06-08 21:05:30', NULL),
-(4, 'tim123456', 'Tim|@123456789', 'Tim', 'John', 'tim42@gmail.com', 'user', '6485db80aeb32.jpg', '1', '2023-06-11 20:07:56', '2023-06-11 20:07:56', NULL);
+(4, 'tim12345', 'Tim|@123456789', 'Tim', 'John', 'tim42@gmail.com', 'user', '6485db80aeb32.jpg', '1', '2023-06-15 09:15:10', '2023-06-15 09:15:10', NULL),
+(5, 'simran1234', 'Simran@12345678', 'SImran', 'Kaur', 'simran@gmail.com', 'user', '648b0e2d87ee6.jpg', '1', '2023-06-15 18:42:13', '2023-06-15 18:42:13', NULL),
+(6, 'john12345', 'John@7894561423', 'John', 'D\'souza', 'john@gmail.com', 'user', '648b0eb138c80.jpg', '1', '2023-06-16 10:52:44', '2023-06-16 10:52:44', NULL);
 
 --
 -- Indexes for dumped tables
@@ -423,13 +428,13 @@ ALTER TABLE `student_table`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `tbl_comments`
 --
 ALTER TABLE `tbl_comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_employeeinfo`
@@ -441,7 +446,7 @@ ALTER TABLE `tbl_employeeinfo`
 -- AUTO_INCREMENT for table `tbl_posts`
 --
 ALTER TABLE `tbl_posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tbl_register`
@@ -453,7 +458,7 @@ ALTER TABLE `tbl_register`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
